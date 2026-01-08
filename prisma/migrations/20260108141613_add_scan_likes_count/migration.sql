f39dd83965d756de8868875137da1796224057bb
1steve78 <yasinsteve4@gmail.com>
2026-01-08 20:03:12 +0530

    fiixng deployement issue

---
+++ prisma/migrations/20260108141613_add_scan_likes_count/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "LearningActivity" ADD COLUMN     "likesCount" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "Scan" ADD COLUMN     "likesCount" INTEGER NOT NULL DEFAULT 0;
